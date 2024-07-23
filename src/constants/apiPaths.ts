7fc3af1ad013ca6ba8cb02f05197c26c48c689b8
Henadzi Shutko <hazardsoft@gmail.com>
2024-07-23 09:32:53 +0300

    feat: update order/cart endpoints urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://hp8wur0lsa.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://6qtdxlqfxf.execute-api.eu-central-1.amazonaws.com/prod/api",
   import: "https://sjova0c3h3.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://6qtdxlqfxf.execute-api.eu-central-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
