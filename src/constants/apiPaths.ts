f9ab731d2beba9ad8321bd18368ee4551a7cde6e
Henadzi Shutko <hazardsoft@users.noreply.github.com>
2024-08-10 10:08:40 +0300

    feat: update api endpoints to connect to EB instance through HTTP API proxy (#7)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://hp8wur0lsa.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://6qtdxlqfxf.execute-api.eu-central-1.amazonaws.com/prod/api",
+  order: "https://lykuq3fkf4.execute-api.eu-central-1.amazonaws.com/api",
   import: "https://sjova0c3h3.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://6qtdxlqfxf.execute-api.eu-central-1.amazonaws.com/prod/api",
+  cart: "https://lykuq3fkf4.execute-api.eu-central-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
